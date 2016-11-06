9b473c2b3aa3503d43ed184f4e9106218c411235
Barbara Stadnik <barbara1stadnik@gmail.com>
2016-11-06 13:19:36 +0100

    scroll effect on mobile

--- js/effects.js
+++ js/effects.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
     function getOffsets() {
         headerHeight = $('#navbarTop').height(); 
+        menuMobileHeight = $('#menuMobile').height() + 50;
     }
 
     $(window).load(getOffsets).resize(getOffsets);
@@ -12,12 +13,35 @@ $(document).ready(function() {
         $('.headerAnchor').click(function () {
             part = $(this).attr('href');
             place = $(part).offset().top - headerHeight;
+            placeMobile = $(part).offset().top - menuMobileHeight;
 
+            if ($(window).width() >= 768) {
             $('html, body').animate({
                 scrollTop: place
             }, 'slow');
+           }   
+
+            if ($(window).width() < 768) { 
+                $('html, body').animate({
+                    scrollTop: placeMobile
+                }, 'slow');
+            }
 
             return false;
         });
     });
+    
+    if ($(window).width() < 768) {
+        $('button').on('click', function(event) {
+            if($('#menuMobile').hasClass('in') == true) {
+                $('#data').animate({
+                    'margin-top': '0'
+                }, 300);
+            } else {
+                $('#data').animate({
+                    'margin-top': '400px'
+                }, 450);
+            }
+        });
+    }
  });
