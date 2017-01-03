e9edb836ce0080413f419a1df1700a7a3ad6ecdb
Barbara Stadnik <barbara1stadnik@gmail.com>
2017-01-03 08:34:25 +0100

    main menu on mobile
    Correct behaviour - when clicking on link in main menu, it should fold
    in

--- js/effects.js
+++ js/effects.js
@@ -13,35 +13,16 @@ $(document).ready(function() {
         $('.headerAnchor').click(function () {
             part = $(this).attr('href');
             place = $(part).offset().top - headerHeight;
-            placeMobile = $(part).offset().top - menuMobileHeight;
 
             if ($(window).width() >= 768) {
             $('html, body').animate({
                 scrollTop: place
             }, 'slow');
-           }   
-
-            if ($(window).width() < 768) { 
-                $('html, body').animate({
-                    scrollTop: placeMobile
-                }, 'slow');
-            }
-
             return false;
         });
     });
     
     if ($(window).width() < 768 || $(window).resize()) {
-        $('button').on('click', function(event) {
-            if($('#menuMobile').hasClass('in') == true) {
-                $('#data').animate({
-                    'margin-top': '0'
-                }, 300);
-            } else {
-                $('#data').animate({ 
-                    'margin-top': '400px'
-                }, 450);
-            }
         });
     }
  });
